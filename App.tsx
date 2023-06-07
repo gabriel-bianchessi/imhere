54f734940fab25a4db642b5eae6c5f68fe8cc20b
Gabriel Bianchessi <gabriel.bianchessi21@gmail.com>
2023-06-06 22:14:15 -0300

    added styles

--- App.tsx
+++ App.tsx
@@ -3,16 +3,28 @@ import { StyleSheet, Text, View, Button, Modal } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: "#cccccc",
-      padding: 24,
-    }}>
-      <Text style={{
-        flex: 1,
-        color: "#000"
-      }}>Open up App.tsx to start working on your aaapp!</Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Evento</Text>
+      <Text style={styles.eventDate}>Sábado, 10 de junho</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    marginTop: 48,
+    fontWeight: "bold",
+  },
+  eventDate: {
+    color: "#6B6B6B",
+    fontSize: 16,
+  }
+})
